80f7b6f9033e9d2b2bc14b426effca3865e29df3
Avelarjr77 <oscaravelarje@gmail.com>
2022-10-15 12:07:24 -0600

    login-laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('Home')
+
+@section('contenido')
+<div class="container">
+    <div class="row">
+        <h1 class="text-5xl text-center pt-24">Login</h1>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!-- Tailwind CSS Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+
+    <!-- Fontawesome Link -->
+    <link href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" rel="stylesheet">
+</head>
+
+<body style="background: url('fondo.png') no-repeat center center fixed; background-size: cover;";>
+    @yield('contenido')
+</body>
+
+</html>
